6de8a05996fb4a11cbd298b9636be44ab6f45c12
dbhor <dbhor1000@gmail.com>
2023-03-08 21:14:59 +0300

    initial commit

---
+++ src/main/java/com/example/homework28proper/Homework28properApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework28proper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework28properApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework28properApplication.class, args);
+    }
+
+}
